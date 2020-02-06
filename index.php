3a0e9e906f8ee9b7d55be8db6ff57031253eec8e
dmitrimeili <47849645+dmitrimeili@users.noreply.github.com>
2020-02-06 16:03:49 +0100

    wip

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
+
 <?php
-
-
+try {
+    $user="ICT-151";
+    $password = 'Pa$$w0rd';
+    $dbh = new PDO('mysql:host=localhost:33066;dbname=mcu', $user, $password);
+    foreach($dbh->query('SELECT * from filmmakers') as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
 ?>
